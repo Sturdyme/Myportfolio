841599fb59b71a9d74c8051534c1cb0d5e9ae521
Sturdyme <emmanuel08135672350@gmail.com>
2026-01-26 07:35:51 -0800

    updated mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { UserProvider } from './component/utilities/UserContext.jsx'
+import { store } from './component/utilities/store.jsx'
 
 createRoot(document.getElementById('root')).render(
+   <Provider store={store}>
   <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
+      <UserProvider>
          <App />
-      </BrowserRouter>
+         </UserProvider>
+      </HashRouter>
        </StrictMode>
+       </Provider>
 )
